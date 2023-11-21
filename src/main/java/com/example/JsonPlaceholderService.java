8f7c067b7f234979eb534eec46eb86ff52ed3178
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2023-11-20 22:45:13 -0300

    Added scaffolding project

---
+++ src/main/java/com/example/JsonPlaceholderService.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.List;
+
+import org.springframework.web.service.annotation.GetExchange;
+
+public interface JsonPlaceholderService {
+
+    @GetExchange("/posts")
+    List<Post> loadPosts();
+}
---
+++ src/main/java/com/example/Post.java
@@ -0,0 +1,3 @@
+package com.example;
+
+public record Post(Integer id, String title, String body) { }
